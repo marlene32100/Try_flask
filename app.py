05708d32e46c6f19ab32f7e8c9bc7b7869ceca48
marlene32100 <katia.debon@hotmail.it>
2020-10-31 21:03:55 +0000

    Added env.py

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ import os
 #we are using data from json so we need to import it
 import json
 #First we import the Flask class
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+
+if os.path.exists("env.py"):
+    import env
 
 # Then we create an instance of this and we store it inside a variable called app.
 # The first argument is the name of the application´s module.
@@ -26,19 +29,23 @@ def about():
     return render_template("about.html", page_title="About", company=data)
 
 
-@app.route("/about/<member_name>")
+@app.route('/about/<member_name>')
 def about_member(member_name):
-    member={}
-    with open("data/company.json", "r") as json.data:
-         data = json.load(json.data)
-         for obj in data:
-             if obj["url"] == member_name:
-                 member = obj
-    return "<h1>" + member["name"] + "</h1>"
+    member = {}
+    
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    
+    return render_template("member.html", member=member)
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        print(request.form)
     return render_template("contact.html", page_title="Contact")
 
 
