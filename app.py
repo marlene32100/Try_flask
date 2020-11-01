fb5032d60509d102415a4372950d367a41851edf
marlene32100 <katia.debon@hotmail.it>
2020-11-01 11:51:48 +0000

    Deployment to Heroku

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 # this is the standard Python library
 import os
-#we are using data from json so we need to import it
+# we are using data from json so we need to import it
 import json
-#First we import the Flask class
+# First we import the Flask class
 from flask import Flask, render_template, request, flash
 
 if os.path.exists("env.py"):
-    import env
+    import env 
+
 
 # Then we create an instance of this and we store it inside a variable called app.
 # The first argument is the name of the application´s module.
 # We are using a single module, so we can use the built-in Python variable "__name__"
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
+
 
 # This is called app.route "decorator". The slash / indicates that we are browsing our root directory.
 @app.route("/")
@@ -21,9 +24,9 @@ def index():
 
 @app.route("/about")
 def about():
-    #first we open an empty list called data
+    # first we open an empty list called data
     data = []
-    #then we tell Python to take data from our json file. This is called "with block"
+    # then we tell Python to take data from our json file. This is called "with block"
     with open("data/company.json", "r") as json.data:
         data = json.load(json.data)
     return render_template("about.html", page_title="About", company=data)
@@ -32,20 +35,21 @@ def about():
 @app.route('/about/<member_name>')
 def about_member(member_name):
     member = {}
-    
+
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
         for obj in data:
             if obj["url"] == member_name:
                 member = obj
-    
+
     return render_template("member.html", member=member)
 
 
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
-        print(request.form)
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
@@ -62,4 +66,3 @@ if __name__ == "__main__":
         # Change it to False before submitting
         debug=True
     )
-    
